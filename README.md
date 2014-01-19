8795d140469db2165ce5debd8f6d03260200d1b5
Samuel Erdtman <samuel@erdtman.se>
2014-01-18 22:52:11 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-strict-transport-security
-=========================
+# strict-transport-security
 
-Node.js middleware to add Strict-Transport-Security header
+Node.js middleware to add Strict-Transport-Security header according to http://tools.ietf.org/html/rfc6797
+
+## Install
+
+    $ npm install strict-transport-security
+
+## Tests
+
+    $ npm install --dev
+    $ npm test
+
+## Usage
+
+### Connect
+
+    var connect = require('connect');
+    var sts = require('strict-transport-security');
+    var server = connect.createServer(sts.getSTS({"max-age":3600}));
+    server.listen(3030);
+    
+### Express
+
+    var sts = require('strict-transport-security');
+    var express = require('express');
+    var app = express();
+    
+    var globalSTS = sts.getSTS({"max-age":{days:30}});
+    var localSTS = sts.getSTS({"max-age":{days:10, includeSubDomains:true}});
+    
+    // Insert before 'app.router'
+    app.use(globalSTS); // This will apply this policy to all requests
+    app.use(app.router);
+    
+    app.get('/settings',
+      localSTS, // This will apply the local policy just to this path
+      function(req, res) {
+        res.render('settings');
+      });
---
+++ test/index.js
@@ -0,0 +1,67 @@
+var assert = require('assert');
+var STS = require('../lib');
+
+suite('Strict Transport Security (STS) middleware', function() {
+  function getRes(result) {
+    return {
+      setHeader : function(name, value) {
+        console.log("set: %s: %s", name, value);
+        result.name = name;
+        result.value = value;
+      }
+    };
+  }
+  var next = function() {
+  };
+
+  test('default', function() {
+    var sts = STS.getSTS();
+    var result = {};
+    sts(null, getRes(result), next);
+
+    assert(result.name === "Strict-Transport-Security");
+    assert(result.value === "max-age=108000000");
+  });
+
+  test('set direct', function() {
+    var sts = STS.getSTS({
+      "max-age" : 12345
+    });
+    var result = {};
+    sts(null, getRes(result), next);
+
+    assert(result.name === "Strict-Transport-Security");
+    assert(result.value === "max-age=12345");
+  });
+
+  test('all time parametes', function() {
+    var sts = STS.getSTS({
+      "max-age" : {
+        milliseconds : 1,
+        seconds : 1,
+        minutes : 1,
+        hours : 1,
+        days : 1
+      }
+    });
+
+    var result = {};
+    sts(null, getRes(result), next);
+
+    assert(result.name === "Strict-Transport-Security");
+    assert(result.value === "max-age=90061001");
+  });
+
+  test('include subdomains', function() {
+    var sts = STS.getSTS({
+      "max-age" : 456789,
+      includeSubDomains : true
+    });
+    var result = {};
+    sts(null, getRes(result), next);
+
+    assert(result.name === "Strict-Transport-Security");
+    assert(result.value === "max-age=456789; includeSubDomains");
+  });
+
+});
