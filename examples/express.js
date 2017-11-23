58913dfa1a74dd7aefcb035604d9a07a8f2c4928
Samuel Erdtman <erdtman@spotify.com>
2017-11-23 22:47:05 +0100

    cleanup

--- examples/express.js
+++ examples/express.js
@@ -1,24 +1,9 @@
-# strict-transport-security
-
-Node.js middleware to add Strict-Transport-Security header according to http://tools.ietf.org/html/rfc6797
-
-## Install
-```
-$ npm install strict-transport-security --save
-```
-## Tests
-```
-$ npm install --dev
-$ npm test
-```
-## Usage
-```js
-const sts = require('strict-transport-security');
+const sts = require('../');
 const express = require('express');
 const app = express();
 
-const globalSTS = sts.getSTS({'max-age':{'days': 30}});
-const localSTS = sts.getSTS({'max-age':{'days': 10}, 'includeSubDomains': true});
+const globalSTS = sts.getSTS({'max-age': {'days': 30}});
+const localSTS = sts.getSTS({'max-age': {'days': 10}, 'includeSubDomains': true});
 
 // This will apply this policy to all requests
 app.use(globalSTS);
@@ -35,4 +20,3 @@ app.get('/local', localSTS, (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
-```
--- test/index.js
+++ test/index.js
@@ -1,66 +1,66 @@
-var assert = require('assert');
-var STS = require('../lib');
+/* jshint esversion: 6 */
+/* jslint node: true */
+'use strict';
 
-suite('Strict Transport Security (STS) middleware', function() {
-  function getRes(result) {
-    return {
-      setHeader : function(name, value) {
-        console.log("set: %s: %s", name, value);
-        result.name = name;
-        result.value = value;
-      }
-    };
-  }
-  var next = function() {
+const test = require('ava');
+const STS = require('../');
+
+function getRes (result) {
+  return {
+    setHeader: (name, value) => {
+      result.name = name;
+      result.value = value;
+    }
   };
+}
 
-  test('default', function() {
-    var sts = STS.getSTS();
-    var result = {};
-    sts(null, getRes(result), next);
+function next () {}
 
-    assert(result.name === "Strict-Transport-Security");
-    assert(result.value === "max-age=2592000");
+test('default', t => {
+  const sts = STS.getSTS();
+  const result = {};
+  sts(null, getRes(result), next);
+
+  t.is(result.name, 'Strict-Transport-Security');
+  t.is(result.value, 'max-age=2592000');
+});
+
+test('set direct', t => {
+  const sts = STS.getSTS({
+    'max-age': 12345
+  });
+  const result = {};
+  sts(null, getRes(result), next);
+
+  t.is(result.name, 'Strict-Transport-Security');
+  t.is(result.value, 'max-age=12345');
+});
+
+test('all time parametes', t => {
+  const sts = STS.getSTS({
+    'max-age': {
+      'seconds': 1,
+      'minutes': 1,
+      'hours': 1,
+      'days': 1
+    }
   });
 
-  test('set direct', function() {
-    var sts = STS.getSTS({
-      "max-age" : 12345
-    });
-    var result = {};
-    sts(null, getRes(result), next);
+  const result = {};
+  sts(null, getRes(result), next);
 
-    assert(result.name === "Strict-Transport-Security");
-    assert(result.value === "max-age=12345");
+  t.is(result.name, 'Strict-Transport-Security');
+  t.is(result.value, 'max-age=90061');
+});
+
+test('include subdomains', t => {
+  const sts = STS.getSTS({
+    'max-age': 456789,
+    'includeSubDomains': true
   });
+  const result = {};
+  sts(null, getRes(result), next);
 
-  test('all time parametes', function() {
-    var sts = STS.getSTS({
-      "max-age" : {
-        seconds : 1,
-        minutes : 1,
-        hours : 1,
-        days : 1
-      }
-    });
-
-    var result = {};
-    sts(null, getRes(result), next);
-
-    assert(result.name === "Strict-Transport-Security");
-    assert(result.value === "max-age=90061");
-  });
-
-  test('include subdomains', function() {
-    var sts = STS.getSTS({
-      "max-age" : 456789,
-      includeSubDomains : true
-    });
-    var result = {};
-    sts(null, getRes(result), next);
-
-    assert(result.name === "Strict-Transport-Security");
-    assert(result.value === "max-age=456789; includeSubDomains");
-  });
-
+  t.is(result.name, 'Strict-Transport-Security');
+  t.is(result.value, 'max-age=456789; includeSubDomains');
 });
